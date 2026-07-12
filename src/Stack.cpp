d9bcb46a42471f93d90de646ddc0872b23b6ffce
Robert Dodson <ravenstudiosrob@yahoo.com>
2026-07-12 17:25:20 -0500

    Added stack

---
+++ src/Stack.cpp
@@ -0,0 +1,17 @@
+#include "Stack.h"
+
+Stack stack;
+
+Stack::Stack(){
+
+}
+
+void Stack::Push(uint16_t address){
+
+}
+
+
+uint16_t Stack::Pop(){
+
+}
+
---
+++ src/Stack.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <array>
+#include <cstdint>
+#include <string>
+
+class Stack{
+    public:
+    Stack();
+    void Push(uint16_t address)
+    uint16_t Pop();
+
+    private:
+        std::array<uint16_t, 16> m_Stack;
+        uint8_t m_StackPointer = 0;
+
+};  
+extern Stack stack;
